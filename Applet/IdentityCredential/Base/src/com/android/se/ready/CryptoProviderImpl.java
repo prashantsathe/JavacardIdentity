e2dddff3385a324468d187a12f72a7dd4f476cd4
Prashant Patil <prashant.patil1@quest-global.com>
2021-08-07 11:33:40 +0530

    CryptoProviderImpl file changes after integration of Keymaster and IdentityCredential in single package. Other files changes are on master branch.

--- Applet/IdentityCredential/Base/src/com/android/se/ready/CryptoProviderImpl.java
+++ Applet/IdentityCredential/Base/src/com/android/se/ready/CryptoProviderImpl.java
@@ -2,134 +2,209 @@ package com.android.se.ready;
 
 import javacard.framework.ISO7816;
 import javacard.framework.ISOException;
+import javacard.framework.JCSystem;
 import javacard.framework.Util;
 import javacard.security.*;
-import javacard.security.KeyPair;
-import javacard.security.Signature;
+import javacardx.crypto.*;
 
-public class CryptoProviderImpl implements ICryptoProvider{
-	private final KMSEProvider kmSEProvider;
+final class CryptoProviderImpl implements ICryptoProvider{
 	private final Signature mHMACSignature;
-	private final KeyPair mECKeyPair1;
+	//private final KeyPair mECKeyPair1;
 	private final KeyAgreement mECDHAgreement;
-	private Signature signerNoDigest;
-	private Signature signerWithSha256;
-	private KeyPair ecKeyPair;
+	private final Signature signerNoDigest;
+	private final Signature signerWithSha256;
+	private final KeyPair ecKeyPair;
+	private final AESKey aesKey;
+
+    private final AEADCipher aesGcmCipher;
+	private final HMACKey mHmacKey;
+	private final byte[] tempBuffer;
 	
+	private KMSEProvider kmSEProvider;
+
 	CryptoProviderImpl() {
-		kmSEProvider = new KMJCardSimulator();
 		mHMACSignature = Signature.getInstance(Signature.ALG_HMAC_SHA_256, false);
-		mECKeyPair1 = new KeyPair(KeyPair.ALG_EC_FP, KeyBuilder.LENGTH_EC_FP_256);
+		/*mECKeyPair1 = new KeyPair(KeyPair.ALG_EC_FP, KeyBuilder.LENGTH_EC_FP_256);
+		ECPrivateKey privKey1 = (ECPrivateKey) mECKeyPair1.getPrivate();
+		Secp256r1.configureECKeyParameters(privKey1);
+		ECPublicKey pubKey1 = (ECPublicKey) mECKeyPair1.getPublic();
+		Secp256r1.configureECKeyParameters(pubKey1);*/
+
 		mECDHAgreement = KeyAgreement.getInstance(KeyAgreement.ALG_EC_SVDP_DH_PLAIN, false);
+
 		ecKeyPair = new KeyPair(KeyPair.ALG_EC_FP, KeyBuilder.LENGTH_EC_FP_256);
+		ECPrivateKey privKey = (ECPrivateKey) ecKeyPair.getPrivate();
+		Secp256r1.configureECKeyParameters(privKey);
+		ECPublicKey pubKey = (ECPublicKey) ecKeyPair.getPublic();
+		Secp256r1.configureECKeyParameters(pubKey);
+
 		signerNoDigest = Signature.getInstance(Signature.ALG_ECDSA_SHA_256, false);
+
 		signerWithSha256 = Signature.getInstance(Signature.ALG_ECDSA_SHA_256, false);
+		aesKey = (AESKey) KeyBuilder.buildKey(
+		        KeyBuilder.TYPE_AES, KeyBuilder.LENGTH_AES_128, false);
+		aesGcmCipher = (AEADCipher) Cipher.getInstance(AEADCipher.ALG_AES_GCM, true);
+
+		mHmacKey = (HMACKey) KeyBuilder.buildKey(KeyBuilder.TYPE_HMAC,
+				(short)256, false);
+
+		tempBuffer = JCSystem.makeTransientByteArray((short)32, JCSystem.CLEAR_ON_RESET);
+		kmSEProvider = KMAndroidSEProvider.getInstance();
 	}
 
-	@Override
 	public void createECKey(byte[] privKeyBuf, short privKeyStart, short privKeyMaxLength,
 			byte[] pubModBuf, short pubModStart, short pubModMaxLength, short[] lengths) {
-		ecKeyPair.genKeyPair();
+		kmSEProvider.createAsymmetricKey(KMType.EC, privKeyBuf, privKeyStart, CryptoManager.EC_KEY_SIZE,
+				pubModBuf, pubModStart, (byte)65, lengths);
 
-		ECPrivateKey privateKey = (ECPrivateKey) ecKeyPair.getPrivate();
-		Secp256r1.configureECKeyParameters(privateKey);
-		lengths[0] = privateKey.getS(privKeyBuf, privKeyStart);
-		ECPublicKey publicKey = (ECPublicKey) ecKeyPair.getPublic();
-		Secp256r1.configureECKeyParameters(publicKey);
-		lengths[1] = publicKey.getW(pubModBuf, pubModStart);
 	}
 
-	@Override
+	public short attestKey(byte[] pubBuf, short pubStart, short pubLength, byte[] keyParams, short keyParamsStart,
+			short keyParamsLength, byte[] outCertbuff, short outCertStart) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
 	public short ecSignWithNoDigest(byte[] privKeyBuf, short privKeyStart, short privKeyLength,
 									byte[] data, short dataStart, short dataLength,
 									byte[] outSign, short outSignStart) {
-
 		ECPrivateKey key = (ECPrivateKey) ecKeyPair.getPrivate();
-		Secp256r1.configureECKeyParameters(key);
 		key.setS(privKeyBuf, privKeyStart, privKeyLength);
 		signerNoDigest.init(key, Signature.MODE_SIGN);
-    	return signerNoDigest.signPreComputedHash(data, dataStart, dataLength, outSign, outSignStart);
+		return signerNoDigest.signPreComputedHash(data, dataStart, dataLength, outSign, outSignStart);
 	}
 
-	@Override
-	public short aesGCMEncrypt(byte[] aesKey, short aesKeyStart, short aesKeyLen, byte[] data, short dataStart,
-			short dataLen, byte[] encData, short encDataStart, byte[] nonce, short nonceStart, short nonceLen,
-			byte[] authData, short authDataStart, short authDataLen, byte[] authTag, short authTagStart,
-			short authTagLen) {
-		return kmSEProvider.aesGCMEncrypt(aesKey, aesKeyStart, aesKeyLen, data, dataStart,
+	public short aesGCMEncrypt(byte[] aesKeyBuff, short aesKeyStart, short aesKeyLen,
+			byte[] data, short dataStart, short dataLen,
+			byte[] encData, short encDataStart,
+			byte[] nonce, short nonceStart, short nonceLen,
+			byte[] authData, short authDataStart, short authDataLen,
+			byte[] authTag, short authTagStart, short authTagLen) {
+
+		KMSEProvider seProvider = kmSEProvider;
+		//if(seProvider != null) {
+		/*	byte[] tempGlobalByteArray = (byte[])JCSystem.makeGlobalArray(JCSystem.ARRAY_TYPE_BYTE, (short)(aesKeyLen + dataLen + dataLen + nonceLen + authDataLen + authTagLen));
+			short aesKeyOffset = (byte)0;
+			short dataOffset = (short)(aesKeyOffset + aesKeyLen);
+			short encDataOffset = (short)(dataOffset + dataLen);
+			short nonceOffset = (short)(encDataOffset + dataLen);
+			short authDataOffset = (short)(nonceOffset + nonceLen);
+			short authTagOffset = (short)(authDataOffset + authDataLen);
+			Util.arrayCopyNonAtomic(aesKeyBuff, aesKeyStart, tempGlobalByteArray, aesKeyOffset, aesKeyLen);
+			Util.arrayCopyNonAtomic(data, dataStart, tempGlobalByteArray, dataOffset, dataLen);
+			Util.arrayCopyNonAtomic(nonce, nonceStart, tempGlobalByteArray, nonceOffset, nonceLen);
+			Util.arrayCopyNonAtomic(authData, authDataStart, tempGlobalByteArray, authDataOffset, authDataLen);
+
+			short encLen = seProvider.aesGCMEncrypt(tempGlobalByteArray, aesKeyOffset, aesKeyLen, tempGlobalByteArray, dataOffset,
+					dataLen, tempGlobalByteArray, encDataOffset, tempGlobalByteArray, nonceOffset, nonceLen,
+					tempGlobalByteArray, authDataOffset, authDataLen, tempGlobalByteArray, authTagOffset, authTagLen);
+
+			Util.arrayCopyNonAtomic(tempGlobalByteArray, encDataOffset, encData, encDataStart, encLen);
+			Util.arrayCopyNonAtomic(tempGlobalByteArray, authTagOffset, authTag, authTagStart, authTagLen);
+			JCSystem.requestObjectDeletion();
+			return encLen;
+		} else {*/
+			this.aesKey.setKey(aesKeyBuff, aesKeyStart);
+			aesGcmCipher.init(this.aesKey, Cipher.MODE_ENCRYPT, nonce, nonceStart, nonceLen);
+
+		    aesGcmCipher.updateAAD(authData, authDataStart, authDataLen);
+		    short ciphLen = aesGcmCipher.doFinal(data, dataStart, dataLen, encData, encDataStart);
+		    aesGcmCipher.retrieveTag(authTag, authTagStart, authTagLen);
+		    return ciphLen;
+		/*}
+		short encLen = seProvider.aesGCMEncrypt(aesKeyBuff, aesKeyStart, aesKeyLen, data, dataStart,
 				dataLen, encData, encDataStart, nonce, nonceStart, nonceLen,
-				authData, authDataStart, authDataLen, authTag, authTagStart,
-				authTagLen);
+				authData, authDataStart, authDataLen, authTag, authTagStart, authTagLen);
+		return encLen;*/
 	}
 
-	@Override
-	public boolean aesGCMDecrypt(byte[] aesKey, short aesKeyStart, short aesKeyLen, byte[] data, short dataStart,
-							   short dataLen, byte[] encData, short encDataStart, byte[] nonce, short nonceStart, short nonceLen,
-							   byte[] authData, short authDataStart, short authDataLen, byte[] authTag, short authTagStart,
-							   short authTagLen) {
-		return kmSEProvider.aesGCMDecrypt(aesKey, aesKeyStart, aesKeyLen,
-				data, dataStart, dataLen,
-				encData, encDataStart,
-				nonce, nonceStart, nonceLen,
-				authData, authDataStart, authDataLen,
-				authTag, authTagStart, authTagLen);
+	public boolean aesGCMDecrypt(byte[] aesKeyBuff, short aesKeyStart, short aesKeyLen,
+			byte[] data, short dataStart, short dataLen,
+			byte[] encData, short encDataStart,
+			byte[] nonce, short nonceStart, short nonceLen,
+			byte[] authData, short authDataStart, short authDataLen,
+			byte[] authTag, short authTagStart, short authTagLen) {
+		KMSEProvider seProvider = kmSEProvider;
+		//if(seProvider != null) {
+		/*	byte[] tempGlobalByteArray = (byte[])JCSystem.makeGlobalArray(JCSystem.ARRAY_TYPE_BYTE, (short)(aesKeyLen + dataLen + dataLen + nonceLen + authDataLen + authTagLen));
+			short aesKeyOffset = (byte)0;
+			short dataOffset = (short)(aesKeyOffset + aesKeyLen);
+			short encDataOffset = (short)(dataOffset + dataLen);
+			short nonceOffset = (short)(encDataOffset + dataLen);
+			short authDataOffset = (short)(nonceOffset + nonceLen);
+			short authTagOffset = (short)(authDataOffset + authDataLen);
+			Util.arrayCopyNonAtomic(aesKeyBuff, aesKeyStart, tempGlobalByteArray, aesKeyOffset, aesKeyLen);
+			Util.arrayCopyNonAtomic(data, dataStart, tempGlobalByteArray, dataOffset, dataLen);
+			Util.arrayCopyNonAtomic(encData, encDataStart, tempGlobalByteArray, encDataOffset, dataLen);
+			Util.arrayCopyNonAtomic(nonce, nonceStart, tempGlobalByteArray, nonceOffset, nonceLen);
+			Util.arrayCopyNonAtomic(authData, authDataStart, tempGlobalByteArray, authDataOffset, authDataLen);
+			Util.arrayCopyNonAtomic(authTag, authTagStart, tempGlobalByteArray, authTagOffset, authTagLen);
+
+			boolean isSuccess = seProvider.aesGCMDecrypt(tempGlobalByteArray, aesKeyOffset, aesKeyLen, tempGlobalByteArray, dataOffset,
+					dataLen, tempGlobalByteArray, encDataOffset, tempGlobalByteArray, nonceOffset, nonceLen,
+					tempGlobalByteArray, authDataOffset, authDataLen, tempGlobalByteArray, authTagOffset, authTagLen);
+
+			Util.arrayCopyNonAtomic(tempGlobalByteArray, encDataOffset, encData, encDataStart, dataLen);
+			JCSystem.requestObjectDeletion();
+			return isSuccess;
+		} else {*/
+			this.aesKey.setKey(aesKeyBuff, aesKeyStart);
+			aesGcmCipher.init(this.aesKey, Cipher.MODE_DECRYPT, nonce, nonceStart, nonceLen);
+
+		    aesGcmCipher.updateAAD(authData, authDataStart, authDataLen);
+		    aesGcmCipher.doFinal(data, dataStart, dataLen, encData, encDataStart);
+		    return aesGcmCipher.verifyTag(authTag, authTagStart, authTagLen, CryptoManager.AES_GCM_TAG_SIZE);
+		/*}
+		boolean isSuccess = seProvider.aesGCMDecrypt(aesKeyBuff, aesKeyStart, aesKeyLen, data, dataStart,
+				dataLen, encData, encDataStart, nonce, nonceStart, nonceLen,
+				authData, authDataStart, authDataLen, authTag, authTagStart, authTagLen);
+
+		return isSuccess;*/
 	}
 
-	@Override
 	public short ecSignWithSHA256Digest(byte[] privKeyBuf, short privKeyStart, short privKeyLength,
 										byte[] data, short dataStart, short dataLength,
 										byte[] outSign, short outSignStart) {
-
 		KMOperation signer = kmSEProvider.initAsymmetricOperation(KMType.SIGN, KMType.EC,  KMType.PADDING_NONE , KMType.SHA2_256,
 				privKeyBuf, privKeyStart, privKeyLength, //Private key
 				privKeyBuf, (short)0, (short)0); //Public key
+		short signLen = signer.sign(data, dataStart, dataLength, outSign, outSignStart);
 
-		return signer.sign(data, dataStart, dataLength, outSign, outSignStart);
+		kmSEProvider.releaseAllOperations();
+		return signLen;
 	}
 
-	@Override
 	public boolean ecVerifyWithNoDigest(byte[] pubKeyBuf, short pubKeyStart, short pubKeyLength,
                                         byte[] data, short dataStart, short dataLength,
                                         byte[] signBuf, short signStart, short signLength) {
 		ECPublicKey pubKey = (ECPublicKey)ecKeyPair.getPublic();
-		Secp256r1.configureECKeyParameters(pubKey);
 		pubKey.setW(pubKeyBuf, pubKeyStart, pubKeyLength);
 		signerNoDigest.init(pubKey, Signature.MODE_VERIFY);
 		return signerNoDigest.verifyPreComputedHash(data, dataStart, dataLength, signBuf, signStart, signLength);
 	}
 
-	@Override
 	public short createECDHSecret(byte[] privKey, short privKeyOffset, short privKeyLen,
 								  byte[] pubKey, short pubKeyOffset, short pubKeyLen,
 								  byte[] outSecret, short outSecretOffset) {
-		ECPrivateKey privateKey = (ECPrivateKey) mECKeyPair1.getPrivate();
-		Secp256r1.configureECKeyParameters(privateKey);
+		ECPrivateKey privateKey = (ECPrivateKey) ecKeyPair.getPrivate();
 		privateKey.setS(privKey, privKeyOffset, privKeyLen);
 		mECDHAgreement.init(privateKey);
-		short result = (short)0;
-		try {
-			result = mECDHAgreement.generateSecret(pubKey, pubKeyOffset, pubKeyLen, outSecret, outSecretOffset);
-		}catch (Exception e){
-			e.printStackTrace();
-		}
+		short result = mECDHAgreement.generateSecret(pubKey, pubKeyOffset, pubKeyLen, outSecret, outSecretOffset);
 		return result;
 	}
 
-	@Override
 	public short hkdf(byte[] sharedSecret, short sharedSecretOffset, short sharedSecretLen,
 					  byte[] salt, short saltOffset, short saltLen,
 					  byte[] info, short infoOffset, short infoLen,
 					  byte[] outDerivedKey, short outDerivedKeyOffset, short expectedDerivedKeyLen) {
 		// HMAC_extract
-		byte[] prk = new byte[32];
-		hkdfExtract(sharedSecret, sharedSecretOffset, sharedSecretLen, salt, saltOffset, saltLen, prk, (short) 0);
+		short prkLen = hkdfExtract(sharedSecret, sharedSecretOffset, sharedSecretLen, salt, saltOffset, saltLen, tempBuffer, (short) 0);
 		//HMAC_expand
-		return hkdfExpand(prk, (short) 0, (short) 32, info, infoOffset, infoLen, outDerivedKey, outDerivedKeyOffset, expectedDerivedKeyLen);
+		return hkdfExpand(tempBuffer, (short) 0, prkLen, info, infoOffset, infoLen, outDerivedKey, outDerivedKeyOffset, expectedDerivedKeyLen);
 	}
 
 	private short hkdfExtract(byte[] ikm, short ikmOff, short ikmLen, byte[] salt, short saltOff, short saltLen,
 							  byte[] out, short off) {
-		// https://tools.ietf.org/html/rfc5869#section-2.2
 		HMACKey hmacKey = createHMACKey(salt, saltOff, saltLen);
 		mHMACSignature.init(hmacKey, Signature.MODE_SIGN);
 		return mHMACSignature.sign(ikm, ikmOff, ikmLen, out, off);
@@ -137,15 +212,14 @@ public class CryptoProviderImpl implements ICryptoProvider{
 
 	private short hkdfExpand(byte[] prk, short prkOff, short prkLen, byte[] info, short infoOff, short infoLen,
 							 byte[] out, short outOff, short outLen) {
-		// https://tools.ietf.org/html/rfc5869#section-2.3
 		short digestLen = (short) 32; // SHA256 digest length.
 		// Calculate no of iterations N.
-		short n = (short) ((outLen + digestLen - 1) / digestLen);
+		short n = (short) ((short)(outLen + digestLen - 1) / digestLen);
 		if (n > 255) {
 			CryptoException.throwIt(CryptoException.ILLEGAL_VALUE);
 		}
 		HMACKey hmacKey = createHMACKey(prk, prkOff, prkLen);
-		byte[] previousOutput = new byte[32]; // Length of output 32.
+		byte[] previousOutput = tempBuffer; // Length of output 32.
 		byte[] cnt = {(byte) 0};
 		short bytesCopied = 0;
 		short len = 0;
@@ -165,26 +239,27 @@ public class CryptoProviderImpl implements ICryptoProvider{
 		return outLen;
 	}
 	public HMACKey createHMACKey(byte[] secretBuffer, short secretOff, short secretLength) {
-		HMACKey key = null;
-		key = (HMACKey) KeyBuilder.buildKey(KeyBuilder.TYPE_HMAC,
-				KeyBuilder.LENGTH_HMAC_SHA_256_BLOCK_64, false);
-		key.setKey(secretBuffer, secretOff, secretLength);
-		return key;
+		mHmacKey.setKey(secretBuffer, secretOff, secretLength);
+		return mHmacKey;
 	}
 
-	@Override
 	public boolean hmacVerify(byte[] key, short keyOffset, short keyLen, byte[] data, short dataOffset, short dataLen, byte[] mac, short macOffset, short macLen) {
 		HMACKey hmacKey = createHMACKey(key, keyOffset, keyLen);
 		mHMACSignature.init(hmacKey, Signature.MODE_VERIFY);
 		return mHMACSignature.verify(data, dataOffset, dataLen, mac, macOffset, macLen);
 	}
 
-	@Override
 	public boolean validateAuthToken(byte[] tokenData, short tokenOffset, short tokenLen) {
-		return false;//TODO Should we support validation HW token from JCard based Keymaster applet?
+		/*KMICInterface kmICInterface = getKMICInterface();
+		byte[] tempGlobalByteArray = (byte[])JCSystem.makeGlobalArray(JCSystem.ARRAY_TYPE_BYTE, tokenLen);
+		Util.arrayCopyNonAtomic(tokenData, tokenOffset, tempGlobalByteArray, (short)0, tokenLen);
+		//return true;
+		boolean result = kmICInterface.validateAuthToken(tempGlobalByteArray, (short)0, tokenLen);
+		JCSystem.requestObjectDeletion();
+		return result;*/
+		return false;//TODO Use Keymaster's hardware auth token validation method
 	}
 
-	@Override
 	public boolean verifyCertByPubKey(byte[] cert, short certOffset, short certLen,
 									  byte[] pubKey, short pubKeyOffset, short pubKeyLen) {
 		if(certLen <= 0 || cert[certOffset] != 0x30) {
@@ -201,14 +276,14 @@ public class CryptoProviderImpl implements ICryptoProvider{
 			ISOException.throwIt(ISO7816.SW_DATA_INVALID);
 		}
 		short tbsLen;
-		if(cert[tbsStart + 1] == (byte)0x81) {
-			tbsLen = (short)(cert[tbsStart + 2] & 0x00FF);
+		if(cert[(short)(tbsStart + 1)] == (byte)0x81) {
+			tbsLen = (short)(cert[(short)(tbsStart + 2)] & 0x00FF);
 			tbsLen += 3;
-		} else if(cert[tbsStart + 1] == (byte)0x82) {
+		} else if(cert[(short)(tbsStart + 1)] == (byte)0x82) {
 			tbsLen = Util.getShort(cert, (short) (tbsStart + 2));
 			tbsLen += 4;
 		} else {
-			tbsLen = (short)(cert[tbsStart + 1] & 0x00FF);
+			tbsLen = (short)(cert[(short)(tbsStart + 1)] & 0x00FF);
 			tbsLen += 2;
 		}
 
@@ -216,10 +291,10 @@ public class CryptoProviderImpl implements ICryptoProvider{
 		if(cert[signSeqStart] != 0x03 && cert[(short)(signSeqStart + (byte)2)] != 0x00) {
 			ISOException.throwIt(ISO7816.SW_DATA_INVALID);
 		}
-		byte signLen = (byte)(cert[signSeqStart + (byte)1] - (byte)1);//Actual signature Bit string starts after 0x00. signature len expected around 70-72
+		byte signLen = (byte)(cert[(short)(signSeqStart + (byte)1)] - (byte)1);//Actual signature Bit string starts after 0x00. signature len expected around 70-72
 
 		ECPublicKey publicKey = (ECPublicKey)ecKeyPair.getPublic();
-		Secp256r1.configureECKeyParameters(publicKey);
+		//Secp256r1.configureECKeyParameters(publicKey);
 		publicKey.setW(pubKey, pubKeyOffset, pubKeyLen);
 		signerWithSha256.init(publicKey, Signature.MODE_VERIFY);
 		return signerWithSha256.verify(cert, tbsStart, tbsLen, cert, (short) (certOffset + certLen - signLen), signLen);
